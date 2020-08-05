8e1a330fab48e3d91c52084d1e2ce4add55419f8
monicadeshmukh <renavikar_monica@yahoo.com>
2020-08-05 16:31:49 -0400

    part1 completed

--- src/main/java/com/github/perschola/MainApplication.java
+++ src/main/java/com/github/perschola/MainApplication.java
@@ -1,8 +1,13 @@
 package com.github.perschola;
 
 public class MainApplication {
-    public static void main(String[] args) {
-        Runnable myObject = new MyObject();
-        myObject.run();
+    public static void main(String[] args) throws InterruptedException {
+        //Runnable myObject = new MyObject();
+        Thread thread1 = new Thread(new MyObject());
+        Thread thread2 = new Thread(new MyObject());
+
+        thread1.start();
+        thread2.start();
+        //myObject.run();
     }
 }
--- src/main/java/com/github/perschola/MyObject.java
+++ src/main/java/com/github/perschola/MyObject.java
@@ -1,7 +1,23 @@
 package com.github.perschola;
+/*Part 1: Write a Java program that starts two threads
+        each thread will print the numbers from 1 - 4.
+        While performing this task each thread will be at sleep for two seconds*/
 
+//import org.jcp.xml.dsig.internal.SignerOutputStream;
+
+import java.util.stream.IntStream;
+
+/**
+ * Implemented By Monica Deshmukh
+ */
 public class MyObject implements Runnable {
     public void run() {
         // TODO
+        IntStream.range(1,5).forEach(System.out::println);
+        try {
+            Thread.sleep(2*1000);
+        }catch (InterruptedException e){
+            System.out.println("Exception occurred: " + e);
+        }
     }
 }
